631f067036cd57439708695d98b3be7e430cf141
Greg Thole <gthole@gmail.com>
2014-11-01 17:33:48 -0400

    Multiple size thumbs

--- watch.rb
+++ watch.rb
@@ -3,7 +3,7 @@ require 'RMagick'
 require 'fileutils'
 
 
-def build_thumb(name)
+def build_thumbs(name)
   if name.downcase[-4..-1] != ".jpg" or name.count("/") != 1
     return
   end
@@ -12,29 +12,33 @@ def build_thumb(name)
   gallery, base = name.split("/")
 
   img = Magick::Image::read("public/galleries/#{name}").first
+  thmb_dir = "public/galleries/#{gallery}/_thumbs/"
+
+  build_thumb(base, img, thmb_dir, 300)
+  build_thumb(base, img, thmb_dir, 600)
+end
+
+
+def build_thumb(base, img, thmb_dir, width)
   w = img.columns
   h = img.rows
 
-  thmb_name = "public/galleries/#{gallery}/_thumbs/#{base}"
+  thmb_name = "#{thmb_dir}/#{width}/#{base}"
   FileUtils.mkdir_p File.dirname(thmb_name)
 
-  nh = (300 / w) * h
-  img.resize_to_fit!(300, nh)
-  img.write(thmb_name)
+  nh = (width / w) * h
+  new = img.resize_to_fit(width, nh)
+  new.write(thmb_name)
 end
 
 
 FSSM.monitor("public/galleries/", '**/*') do
   update do |b, r, t|
-    build_thumb(r)
+    build_thumbs(r)
   end
 
   create do |b, r, t|
-    build_thumb(r)
-  end
-
-  delete do |b, r, t|
-    puts "Delete thumb for #{r}"
+    build_thumbs(r)
   end
 end
 
